5c0f05b1760e483bd81eb4d849f1cf7e85c2b595
1f10240283 <s1f102402833@iniad.org>
2025-07-10 14:06:23 +0900

    削除機能を追加

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import Http404
 from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
@@ -27,6 +27,8 @@ def detail(request, task_id):
         task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
+    task.delete()
+    return redirect(index)
 
     context = {
         'task': task,
